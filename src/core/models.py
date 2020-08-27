7bb0bdfe493d4e3172b356eb7606b655ddccac5c
Dahalia Howell <dahaliahowell.dh@gmail.com>
2020-08-27 10:39:06 -0500

    Added ImageField for posters to the event model and modified the event unit test

--- src/core/models.py
+++ src/core/models.py
@@ -33,6 +33,8 @@ class Event(models.Model):
     start_date_time = models.DateTimeField()
     end_date_time = models.DateTimeField()
     location = models.CharField(max_length = 150)
+    poster_image = models.ImageField(upload_to = 'assets/', height_field = None,\
+        width_field = None, max_length = 100, default = '')
 
     def __str__(self):
         return self.name
